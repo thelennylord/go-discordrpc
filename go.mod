a33b0656d2a3ef9d247fca30d19d145e11ad2476
Lenny Lord <32369619+thelennylord@users.noreply.github.com>
2023-03-29 17:52:50 +0530

    Remove npipe and use go-winio

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/rikkuness/discord-rpc
 go 1.13
 
 require (
+	github.com/Microsoft/go-winio v0.6.0
 	github.com/google/uuid v1.1.1
-	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce
 )
--- ipc/ipc_windows.go
+++ ipc/ipc_windows.go
@@ -1,3 +1,4 @@
+//go:build windows
 // +build windows
 
 package ipc
@@ -5,7 +6,7 @@ package ipc
 import (
 	"time"
 
-	npipe "gopkg.in/natefinch/npipe.v2"
+	"github.com/Microsoft/go-winio"
 )
 
 // NewConnection opens the discord-ipc-0 named pipe
@@ -13,7 +14,8 @@ func NewConnection() (*Socket, error) {
 	// Connect to the Windows named pipe, this is a well known name
 	// We use DialTimeout since it will block forever (or very very long) on Windows
 	// if the pipe is not available (Discord not running)
-	sock, err := npipe.DialTimeout(`\\.\pipe\discord-ipc-0`, time.Second*2)
+	t := 2 * time.Second
+	sock, err := winio.DialPipe(`\\.\pipe\discord-ipc-0`, &t)
 	if err != nil {
 		return nil, err
 	}
